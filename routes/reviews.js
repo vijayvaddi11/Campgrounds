2bdcc9d80c1c74e0fc62f121ac400ccb0881f4da
vijay vaddi <vijayjay6688@gmail.com>
2025-06-04 20:39:40 +0530

    add review submission and deletion routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -8,7 +8,7 @@ const {reviewSchema } = require('../schemas');
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 
-
+ 
 const validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
   if (error) {
